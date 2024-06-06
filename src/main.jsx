74241d97f8e4236890f84e18fb16173353096b33
SABELO MAWELA <MAWELAS63@GMAIL.COM>
2024-06-06 10:27:35 +0200

    "Added miragejs dependency and imported it in main.jsx, also added new route for /vans"

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./home"
 import About from "./about"
+import "./server"  
+import vans from "./vans"
 
 
 function App() {
@@ -12,9 +14,11 @@ function App() {
         <Link className="site-logo" to="/">#VanLife</Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to="/vans">vans</Link>
         </nav>
       </header>
       <Routes>
+        <Route path='/vans' element={<vans />}/>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
---
+++ src/vans.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+
+export default function vans() {
+    return (
+       <h1>vans page goes here</h1> 
+    )
+}
