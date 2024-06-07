a4d52810da59b43c7b5277c6e1c42bd441335424
SABELO MAWELA <MAWELAS63@GMAIL.COM>
2024-06-07 16:45:46 +0200

    "Updated Header component to use NavLink instead of Link, added active link styles, and modified Layout component to wrap content in a div with a class."

--- components/header.jsx
+++ components/header.jsx
@@ -1,15 +1,20 @@
 import React from "react"
-import { Link } from "react-router-dom"
+import { Link,NavLink } from "react-router-dom"
 
 
 export default function Header() {
+  const isActive = {
+    fontWeight: "bold",
+    textDecoration:"none" ,
+    color: '#161616',
+  };
 return (
 <header>
         <Link className="site-logo" to="/">#VanLife</Link>
         <nav>
-            <Link to="/host">Host</Link>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
+          <NavLink to="/host"className={({isActive}) => isActive ? "activeLink" :null }>Host</NavLink>
+          <NavLink to="/about" className={({isActive}) => isActive ? "activeLink" :null }>About</NavLink>
+          <NavLink to="/vans" className={({isActive}) => isActive ? "activeLink" :null }>Vans</NavLink>
         </nav>
       </header>)
       
--- components/layout.jsx
+++ components/layout.jsx
@@ -7,12 +7,14 @@ import Footer from './footer'
 
 export default function Layout() {
     return (
-        <>
+        <div className="site-wrapper">
         <Header />
+        <main>
         <Outlet />
+        </main>
         <Footer />
         
-        </>
+        </div>
         
     )
 }
